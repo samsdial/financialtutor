7847230452a583b0ae964638458688dbf3f612b0
Sergio Andres Martinez E <sergio.martinez@fracttal.com>
2024-10-21 09:49:01 -0500

    changes front end, and others

--- tracker/test/conftest.py
+++ tracker/test/conftest.py
@@ -1,6 +1,11 @@
 import pytest
-from tracker.factories import TransactionFactory
+from tracker.factories import TransactionFactory, UserFactory
 
 @pytest.fixture
 def transactions():
     return TransactionFactory.create_batch(20)
+
+@pytest.fixture
+def user_transactions():
+    user = UserFactory()
+    return TransactionFactory.create_batch(20, user=user)
--- tracker/views.py
+++ tracker/views.py
@@ -15,7 +15,14 @@ def transactions_list(request):
         request.GET,
         queryset=Transaction.objects.filter(user=request.user).select_related('category')
     )
-    context = {'filter': transaction_filter}
+    total_income = transaction_filter.qs.get_total_incomes()
+    total_expense = transaction_filter.qs.get_total_expenses()
+    context = {
+        'filter': transaction_filter,
+        'total_income': total_income,
+        'total_expense': total_expense,
+        'net_income': total_income - total_expense,
+    }
     if request.htmx:
         return render(request, 'tracker/partials/transactions-container.html', context)
 
